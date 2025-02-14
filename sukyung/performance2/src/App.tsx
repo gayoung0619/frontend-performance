69355e3ec9971dffc0c3df0594da158cdaabfeb4
sukyung6999 <sukyung6999@gmail.com>
2025-02-14 21:34:14 +0900

    modify: 기존의 lcp 개선 방법 적용해보기

--- sukyung/performance2/src/App.tsx
+++ sukyung/performance2/src/App.tsx
@@ -1,9 +1,9 @@
-import image from './assets/image.jpg';
+import image from './assets/image.webp';
 import './App.css';
 
 function App() {
   const images = Array.from({ length: 100 }, (_, index) => (
-      <img key={index} src={image} width="100%" />
+    <img key={index} src={image} width="100%" loading="lazy" />
   ));
 
   return <div className="image-grid">{images}</div>;
